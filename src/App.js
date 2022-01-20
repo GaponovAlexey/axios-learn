c0bf282d4f541e32ea58346a7822fa564162cbf0
Kekelberi <alexei.gaponov@gmail.com>
2022-01-20 22:00:00 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [state, setSTATE] = useState([]);
+  
+
+useEffect(() => {
+ axios.get('https://fortnite-api.com/v1/map')
+ .then(res => console.log(res))
+},[])
+
   return (
     <div >
-      ax
+      {state.map(el => <div>{el.data}</div>)}
     </div>
   );
 }
